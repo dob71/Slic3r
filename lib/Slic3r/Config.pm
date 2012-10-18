96187ce30227409c6167dd154ebd7a608254898a
Denis Bakin <denisb@elbrys.com>
2012-10-18 14:47:00 -0400

    Re-fix for binary mode and startup in non-x2sw mode

--- lib/Slic3r/Config.pm
+++ lib/Slic3r/Config.pm
@@ -5,6 +5,7 @@ use utf8;
 
 use File::Basename;
 use File::HomeDir;
+use Wx qw();
 
 use constant PI => 4 * atan2(1, 1);
 
@@ -1147,7 +1148,9 @@ sub read_ini {
 
 sub GetConfigDir {
     # If we are not included into the X2SW bundle, use standard path
+    my $slic3rpath = dirname($FindBin::Bin =~ s/\/bin$//);
     my $x2sw_bundle_prof_path = dirname($FindBin::Bin) . '/.x2sw';
+    Slic3r::debugf "Looking for $x2sw_bundle_prof_path\n";
     if(! -e $x2sw_bundle_prof_path) { 
         Slic3r::debugf "Using standard config path\n";
         return Wx::StandardPaths::Get->GetUserDataDir;
